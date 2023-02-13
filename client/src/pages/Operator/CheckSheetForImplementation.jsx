fd1ff7f0aec148271abe1875719bf9f4a3b2476b
romin301 <romin301.osl@gmail.com>
2023-02-13 17:25:12 +0530

    add build

--- client/src/pages/Operator/CheckSheetForImplementation.jsx
+++ client/src/pages/Operator/CheckSheetForImplementation.jsx
@@ -1054,11 +1054,24 @@ const CheckSheet = ({
                             (MTD HOD)
                           </th>
                           <td className="ar-table-col1" colSpan={6}>
-                            {machineAllData?.checkSheet_data?.implementation_approved_by_MTD_HOD?.Sep?.[machineAllData?.checkSheet_data?.implementation_approved_by_MTD_HOD?.Sep?.length -1]}
+                            {
+                              machineAllData?.checkSheet_data
+                                ?.implementation_approved_by_MTD_HOD?.Sep?.[
+                                machineAllData?.checkSheet_data
+                                  ?.implementation_approved_by_MTD_HOD?.Sep
+                                  ?.length - 1
+                              ]
+                            }
                           </td>
                           <td className="ar-table-col1" colSpan={6}>
-                            {machineAllData?.checkSheet_data?.implementation_approved_by_MTD_HOD?.Mar?.[machineAllData?.checkSheet_data?.implementation_approved_by_MTD_HOD?.Mar?.length -1]
-                            )}
+                            {
+                              machineAllData?.checkSheet_data
+                                ?.implementation_approved_by_MTD_HOD?.Mar?.[
+                                machineAllData?.checkSheet_data
+                                  ?.implementation_approved_by_MTD_HOD?.Mar
+                                  ?.length - 1
+                              ]
+                            }
                           </td>
                         </tr>
                       </thead>
