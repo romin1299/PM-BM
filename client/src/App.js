fe04ad24aa8daf9211afdcd279831eed2082423a
hetal.osl17 <hetal.osl17@gmail.com>
2023-10-17 10:32:10 +0530

    Created tabs and requestsheet

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PMTabdashboard from './Tabs/PMTabdashboard';
-import BMTabDashboard from './Tabs/BMTabDashboard'
+// import BMTabDashboard from './Tabs/BMTabDashboard'
+import Checksheet from './Tabs/RequestSheet'
+
 // ... (import other components as needed)
 
 function Dashboard1() {
   return (
     <div className="scrollable-tabs-container">
-      <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+      <Tabs defaultActiveKey={3} id="uncontrolled-tab-example">
         <Tab eventKey={1} title="Preventive Maintenance">
         <PMTabdashboard />
         </Tab>
+        
         <Tab eventKey={2} title="Breakdown Maintenance">
-          <BMTabDashboard />
+          <Checksheet />
         </Tab>
         
         {/* Add more tabs as needed */}
