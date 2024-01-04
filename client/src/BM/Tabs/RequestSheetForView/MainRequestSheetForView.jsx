558c84957a052aefbc4c8375f737fb04c9f0d91b
mihir <mihir.osl@outlook.com>
2024-01-04 13:44:35 +0530

    View request sheet page without input fields

---
+++ client/src/BM/Tabs/RequestSheetForView/MainRequestSheetForView.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState, useContext } from "react";
+import PRDRequestSheetForUpdate from "./PRDRequestSheetForView";
+import { useParams, useNavigate } from "react-router-dom";
+import RoutingContext from "../../../context/routing/RoutingContext";
+import MTDRequestSheet from "./MTDRequestSheetForView";
+
+function MyTable() {
+  const navigate = useNavigate();
+  const context = useContext(RoutingContext);
+
+  const { machine_code, requestSheetID, generateType } = useParams();
+  const [selectedMachineDetails, setMachineDetails] = useState("");
+  const [requestSheetDataOfBM, setRequestSheetDataOfBM] = useState("");
+
+  const [approvalListOfBM, setApprovalListOfBM] = useState([]);
+  const [supportingTMList, setSupportingTMList] = useState([]);
+  const getMachineDetails = async () => {
+    try {
+      const res = await fetch(
+        `/getMachineDetailsOnScanningRequest/${generateType}/?machine_code=${machine_code}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+      if (res.status === 404) {
+        if (generateType === "scanned") {
+          navigate("/", { replace: true });
+        } else {
+          navigate("/bm/generateRequestSheetMainDashboard", { replace: true });
+        }
+      } else {
+        const { machine, requestSheetApprovalList } = await res.json();
+        // setMachine(machine);
+        // console.log(machine);
+
+        setMachineDetails(machine);
+        setApprovalListOfBM(requestSheetApprovalList);
+        // setSelectedAttendee(breakDownAttendedBy);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getRequestSheetDetails = async () => {
+    try {
+      const res = await fetch(
+        `/getMachineRequestSheetDetails/?_id=${requestSheetID}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+      const data = await res.json();
+      if (res.status === 404) {
+        console.log("error", data?.message);
+      } else {
+        setRequestSheetDataOfBM(data?.requestSheetData?.[0]);
+        setSupportingTMList(data?.TLHOSS_and_TM_user_list);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMachineDetails();
+  }, [machine_code]);
+
+  useEffect(() => {
+    getRequestSheetDetails();
+  }, [requestSheetID]);
+
+  return (
+    <>
+      <div style={{ overflow: "scroll" }}>
+        <PRDRequestSheetForUpdate
+          // selectedMachineDetails={selectedMachineDetails}
+          requestSheetDataOfBM={requestSheetDataOfBM}
+          // approvalListOfBM={approvalListOfBM}
+        />
+
+        {/* need to add condition for PRD not able add data on MTD part */}
+        <MTDRequestSheet
+          selectedMachineDetails={selectedMachineDetails}
+          approvalListOfBM={approvalListOfBM}
+          requestSheetDataOfBM={requestSheetDataOfBM}
+          supportingTMList={supportingTMList}
+        />
+      </div>
+    </>
+  );
+}
+
+export default MyTable;
--- client/src/BM/Tabs/RequestSheetForView/PRDRequestSheetForView.jsx
+++ client/src/BM/Tabs/RequestSheetForView/PRDRequestSheetForView.jsx
@@ -10,16 +10,17 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import { Box } from "@mui/material";
 
 import moment from "moment-timezone";
 import { ToastContainer } from "react-toastify";
 import { useParams } from "react-router-dom";
 import { SuccessToast, WarningToast } from "../../Component/ShowTostify";
 import RoutingContext from "../../../context/routing/RoutingContext";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
-import MachineInfoBox from "../RequestSheetForView/SubComponents/MachineInfoBox";
+import MachineInfoBox from "./SubComponents/MachineInfoBox";
 
 const list = [
   { key: "A", value: "A" },
@@ -90,54 +91,9 @@ function MyTable({ requestSheetDataOfBM }) {
     fetchShiftData();
   }, []);
 
-  const newRequestSheetRegistration = async (requestSheetData) => {
-    // const machineRef = "63b67ccea716e21c95cd471a";
-    // requestSheetData.maintenanceType = selectedMaintenanceType;
-    // requestSheetData.priorityCode = selectedPriorityCode;
-    // requestSheetData.qualityRelated = selectedQuality;
-    // requestSheetData.shiftOfBM = selectedShift;
-    const formData = new FormData();
-    const { ...otherFields } = requestSheetData;
-
-    formData.append("prdDataUpdatedByOtherUser", true);
-    formData.append("otherData", JSON.stringify(otherFields));
-
-    try {
-      const res = await fetch(
-        `/newRequestSheetRegistration/?reqId=${requestSheetID}&&machineRef=${machine_code}`,
-        {
-          method: "POST",
-          // headers: {
-          //   "Content-Type": "application/json",
-          // },
-          body: formData,
-        }
-      );
-
-      const data = await res.json();
-
-      if (res.status === 201) {
-        SuccessToast(data?.message);
-        reset();
-        if (generateType === "scanned") {
-          navigate("/", { replace: true });
-        } else if (
-          requestSheetDataOfBM?.assignUser?._id === loggedUserDetails?._id
-        ) {
-          navigate("/bm/requestListDashboard", { replace: true });
-        } else {
-          navigate("/bm/approval", { replace: true });
-        }
-      } else {
-        WarningToast(data?.message);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
     if (requestSheetDataOfBM?._id) {
+      console.log("requestSheetDataOfBM:", requestSheetDataOfBM);
       setValue(
         "problemOccurredDateAndTimeOfBM",
         moment(requestSheetDataOfBM?.problemOccurredDateAndTimeOfBM)
@@ -190,24 +146,14 @@ function MyTable({ requestSheetDataOfBM }) {
   }, [requestSheetDataOfBM?._id, setValue]);
 
   const handleBack = () => {
-    if (requestSheetDataOfBM?.assignUser?._id === loggedUserDetails?._id) {
-      navigate("/bm/requestListDashboard", { replace: true });
-    } else {
-      navigate("/bm/approval", { replace: true });
-    }
+    navigate("/bm/report/productionLineWiseReport", { replace: true });
   };
 
   return (
     <>
       <ToastContainer />
-      <Row>
-        {/* <Col>
-          <button className="btn bg-button m-2" onClick={handleBack}>
-            Back
-          </button>
-        </Col> */}
-      </Row>
-      <form onSubmit={handleSubmit(newRequestSheetRegistration)}>
+
+      <form onSubmit={() => {}}>
         <Table className="m-2 mt-3">
           <thead>
             {/* <tr>
@@ -225,7 +171,7 @@ function MyTable({ requestSheetDataOfBM }) {
                 alt="React Bootstrap logo"
               />
             </td> */}
-              <td class="col-lg-12 col-md-12 col-sm-12 border-bottom-0">
+              <td class="col-lg-12 col-md-12 col-sm-12 border-bottom-0 position-relative">
                 <Row>
                   <Col>
                     <button className="btn bg-button m-2" onClick={handleBack}>
@@ -256,148 +202,24 @@ function MyTable({ requestSheetDataOfBM }) {
                 </Row>
               </td>
             </tr>
+
             <tr className="row m-2" style={{ width: "100vw" }}>
               <td className="mb-0 pb-0 border col-lg-1 col-md-2">
                 <small>
                   <b>MAINT. TYPE</b>
                 </small>
-                <Form style={{ fontSize: "16px !important" }}>
-                  <div key={`inline-radio`}>
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="BM"
-                      name="maintenanceType"
-                      type="radio"
-                      id={`inline-radio-1`}
-                      value="BM"
-                      {...register("maintenanceType", {
-                        required: "Please select maintenance type",
-                      })}
-                      // onChange={handleMaintenanceType}
-                      // checked={selectedMaintenanceType === "BM"}
-                    />
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="PM"
-                      name="maintenanceType"
-                      type="radio"
-                      id={`inline-radio-2`}
-                      value="PM"
-                      // onChange={handleMaintenanceType}
-                      // checked={selectedMaintenanceType === "PM"}
-                      {...register("maintenanceType", {
-                        required: "Please select maintenance type",
-                      })}
-                    />
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="CM"
-                      type="radio"
-                      name="maintenanceType"
-                      id={`inline-radio-3`}
-                      value="CM"
-                      // onChange={handleMaintenanceType}
-                      // checked={selectedMaintenanceType === "CM"}
-                      {...register("maintenanceType", {
-                        required: "Please select maintenance type",
-                      })}
-                    />
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="TPM"
-                      type="radio"
-                      name="maintenanceType"
-                      id={`inline-radio-4`}
-                      value="TPM"
-                      // onChange={handleMaintenanceType}
-                      // checked={selectedMaintenanceType === "TPM"}
-                      {...register("maintenanceType", {
-                        required: "Please select maintenance type",
-                      })}
-                    />
-                  </div>
-                  {errors?.["maintenanceType"] && (
-                    <p className="text-error">
-                      {errors?.["maintenanceType"]?.message}
-                    </p>
-                  )}
-                </Form>
+                <div className="value">
+                  {requestSheetDataOfBM.maintenanceType}
+                </div>
               </td>
+
               <td className="mb-0 pb-0 border col-lg-1 col-md-2">
                 <small>
-                  {" "}
                   <b>PRIORITY CODE</b>
                 </small>
-                <Form>
-                  <div key={`inline-radio`}>
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="EMERGENCY"
-                      name="priorityCode"
-                      type="radio"
-                      id={`inline-radio-1`}
-                      value="EMERGENCY"
-                      // onChange={handlePriorityCode}
-                      // checked={selectedPriorityCode === "EMERGENCY"}
-                      {...register("priorityCode", {
-                        required: "Please select priority code",
-                      })}
-                    />
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="IMPORTANT"
-                      name="priorityCode"
-                      type="radio"
-                      id={`inline-radio-2`}
-                      value="IMPORTANT"
-                      // onChange={handlePriorityCode}
-                      // checked={selectedPriorityCode === "IMPORTANT"}
-                      {...register("priorityCode", {
-                        required: "Please select priority code",
-                      })}
-                    />
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="DATA NEEDED"
-                      name="priorityCode"
-                      type="radio"
-                      id={`inline-radio-3`}
-                      value="DATA NEEDED"
-                      // onChange={handlePriorityCode}
-                      // checked={selectedPriorityCode === "DATA NEEDED"}
-                      {...register("priorityCode", {
-                        required: "Please select priority code",
-                      })}
-                    />
-                    <Form.Check
-                      flex
-                      style={{ fontSize: "12px" }}
-                      label="KAIZEN"
-                      name="priorityCode"
-                      type="radio"
-                      id={`inline-radio-4`}
-                      value="KAIZEN"
-                      // onChange={handlePriorityCode}
-                      // checked={selectedPriorityCode === "KAIZEN"}
-                      {...register("priorityCode", {
-                        required: "Please select priority code",
-                      })}
-                    />
-                  </div>
-                  {errors?.["priorityCode"] && (
-                    <p className="text-error">
-                      {errors?.["priorityCode"]?.message}
-                    </p>
-                  )}
-                </Form>
+                <div className="value">{requestSheetDataOfBM.priorityCode}</div>
               </td>
+
               <td className="mb-0 pb-0 border col-lg-8 col-md-4">
                 <div className="mb-2 border">
                   <Row className="m-0">
@@ -578,6 +400,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={3}>
                     <input
+                      disabled={true}
                       type="text"
                       id="prob"
                       name="problemFaced"
@@ -604,6 +427,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={3}>
                     <input
+                      disabled={true}
                       type="text"
                       id="prdobv"
                       name="prdobv"
@@ -631,6 +455,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={7}>
                     <input
+                      disabled={true}
                       type="text"
                       id="why"
                       name="why"
@@ -653,6 +478,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={7}>
                     <input
+                      disabled={true}
                       type="text"
                       id="where"
                       name="where"
@@ -675,6 +501,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={7}>
                     <input
+                      disabled={true}
                       type="text"
                       id="when"
                       name="when"
@@ -697,6 +524,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={7}>
                     <input
+                      disabled={true}
                       type="text"
                       id="who"
                       name="who"
@@ -719,6 +547,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={7}>
                     <input
+                      disabled={true}
                       type="text"
                       id="which"
                       name="which"
@@ -741,6 +570,7 @@ function MyTable({ requestSheetDataOfBM }) {
                   </Col>
                   <Col lg={7}>
                     <input
+                      disabled={true}
                       type="text"
                       id="how"
                       name="how"
@@ -795,36 +625,7 @@ function MyTable({ requestSheetDataOfBM }) {
                     <small className="mb-0 d-flex align-items-center justify-content-start">
                       <b>QUALITY RELATED</b>&nbsp;&nbsp;&nbsp;
                     </small>
-                  </Col>
-                  <Col className="border p-2 d-flex align-items-center">
-                    <Form>
-                      {["radio"].map((type) => (
-                        <div key={`inline-${type}`} className="d-block">
-                          <Form.Check
-                            flex
-                            label="Yes"
-                            name="group1"
-                            type={type}
-                            id={`inline-${type}-1`}
-                            value="Yes"
-                            {...register("qualityRelated", {
-                              required: "Please select quality related",
-                            })}
-                          />
-                          <Form.Check
-                            flex
-                            label="No"
-                            name="group1"
-                            type={type}
-                            id={`inline-${type}-2`}
-                            value="No"
-                            {...register("qualityRelated", {
-                              required: "Please select quality related",
-                            })}
-                          />
-                        </div>
-                      ))}
-                    </Form>
+                    <div>{requestSheetDataOfBM?.qualityRelated}</div>
                   </Col>
                 </Row>
                 <Row className="pt-0 mb-0 m-0">
@@ -842,27 +643,8 @@ function MyTable({ requestSheetDataOfBM }) {
               </td>
             </tr>
           </tbody>
-          {loggedUserDetails?.tm_department === "MTD" ||
-          (requestSheetDataOfBM?.assignUser?._id === loggedUserDetails?._id &&
-            (requestSheetDataOfBM?.requestSheetStatus === "Fill Sheet" ||
-              requestSheetDataOfBM?.requestSheetStatus ===
-                "Work Order Pending" ||
-              requestSheetDataOfBM?.requestSheetStatus ===
-                "Work Order Closed")) ? (
-            <Row>
-              <Col>
-                <button
-                  type="submit"
-                  className="btn bg-warning"
-                  style={{ marginTop: "1rem" }}
-                >
-                  Update Filled PRD Data
-                </button>
-              </Col>
-            </Row>
-          ) : (
-            ""
-          )}
+
+          <div style={{ width: "100%", height: "50px" }}></div>
         </Table>
       </form>
     </>
