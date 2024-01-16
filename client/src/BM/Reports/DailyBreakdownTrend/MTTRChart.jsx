d4a3c1c536deb8b91a970b8c8e60d7af4ebcf768
JD <jd.optimizesolutions@gmail.com>
2024-01-16 10:31:53 +0530

    sc

--- client/src/BM/Reports/DailyBreakdownTrend/MTTRChart.jsx
+++ client/src/BM/Reports/DailyBreakdownTrend/MTTRChart.jsx
@@ -90,87 +90,11 @@ const labels = [
   "Mar",
 ];
 
-export const initialData = {
-  labels,
-  datasets: [
-    {
-      type: "line",
-      label: "Average",
-      data: [12, 18, 15, 5, 23, 18, 20, 10, 8, 16, 20, 15],
-      borderColor: chartColors.blue[1],
-      borderWidth: 2,
-      backgroundColor: chartColors.blue[1],
-      pointStyle: "rectRot",
-      yAxisID: "y1",
-    },
-    {
-      type: "bar",
-      stack: "bar-stacked",
-      label: "FP",
-      data: [3, 15, 10, 8, 12, 18, 20, 25, 30, 5, 15, 10],
-      backgroundColor: chartColors.yellow[1],
-      borderColor: chartColors.yellow[1],
-      pointStyle: "rect",
-    },
-    {
-      type: "bar",
-      stack: "bar-stacked",
-      label: "INJ",
-      data: [20, 8, 15, 10, 5, 18, 12, 25, 30, 3, 10, 15],
-      backgroundColor: chartColors.aqua[3],
-      borderColor: chartColors.aqua[3],
-      pointStyle: "rect",
-    },
-    {
-      type: "bar",
-      stack: "bar-stacked",
-      label: "VCT",
-      data: [10, 15, 20, 8, 5, 25, 18, 30, 12, 3, 15, 10],
-      backgroundColor: chartColors.purple[4],
-      borderColor: chartColors.purple[4],
-      pointStyle: "rect",
-    },
-    {
-      type: "bar",
-      stack: "bar-stacked",
-      label: "O2",
-      data: [15, 10, 8, 20, 18, 5, 12, 25, 30, 3, 15, 10],
-      backgroundColor: chartColors.green[3],
-      borderColor: chartColors.green[3],
-      pointStyle: "rect",
-    },
-    {
-      type: "bar",
-      stack: "bar-stacked",
-      label: "ETB",
-      data: [8, 15, 10, 5, 18, 20, 25, 30, 12, 3, 15, 10],
-      backgroundColor: chartColors.magenta[2],
-      borderColor: chartColors.magenta[2],
-      pointStyle: "rect",
-    },
-    {
-      type: "bar",
-      stack: "bar-stacked",
-      label: "VCT PARTS",
-      data: [10, 5, 20, 8, 12, 15, 18, 30, 3, 25, 15, 10],
-      backgroundColor: chartColors.blue[3],
-      borderColor: chartColors.blue[3],
-      pointStyle: "rect",
-    },
-    {
-      type: "bar",
-      stack: "bar-stacked",
-      label: "FP PARTS",
-      data: [5, 15, 10, 8, 12, 18, 20, 25, 30, 3, 15, 10],
-      backgroundColor: chartColors.brown[1],
-      borderColor: chartColors.brown[1],
-      pointStyle: "rect",
-    },
-  ],
-};
-
 const MTTRChart = ({ flagForTogglingFilter, selectedValue, selectedYear }) => {
-  const [mttrData, setMttrData] = useState([]);
+  const [mttrData, setMttrData] = useState({
+    cellWiseGroupData: [{ label: "", data: [] }],
+    avgData: [],
+  });
 
   const getMTTRData = async () => {
     try {
@@ -189,7 +113,7 @@ const MTTRChart = ({ flagForTogglingFilter, selectedValue, selectedYear }) => {
       const { message, mttrData } = await res.json();
 
       if (res?.status === 201) {
-        console.log(mttrData);
+        setMttrData(mttrData);
       }
     } catch (error) {
       console.log(error);
@@ -200,12 +124,102 @@ const MTTRChart = ({ flagForTogglingFilter, selectedValue, selectedYear }) => {
     if (selectedValue) getMTTRData();
   }, [selectedValue, selectedYear]);
 
+  console.log(mttrData)
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        type: "line",
+        label: "Average",
+        data: mttrData?.avgData,
+        borderColor: chartColors.blue[1],
+        borderWidth: 2,
+        backgroundColor: chartColors.blue[1],
+        pointStyle: "rectRot",
+        yAxisID: "y1",
+      },
+      ...mttrData?.cellWiseGroupData?.map((item) => ({
+        type: "bar",
+        stack: "bar-stacked",
+        label: item?.label,
+        data: item?.data,
+        backgroundColor: chartColors.yellow[1],
+        borderColor: chartColors.yellow[1],
+        pointStyle: "rect",
+      })),
+      // {
+      //   type: "bar",
+      //   stack: "bar-stacked",
+      //   label: "FP",
+      //   data: [3, 15, 10, 8, 12, 18, 20, 25, 30, 5, 15, 10],
+      //   backgroundColor: chartColors.yellow[1],
+      //   borderColor: chartColors.yellow[1],
+      //   pointStyle: "rect",
+      // },
+      // {
+      //   type: "bar",
+      //   stack: "bar-stacked",
+      //   label: "INJ",
+      //   data: [20, 8, 15, 10, 5, 18, 12, 25, 30, 3, 10, 15],
+      //   backgroundColor: chartColors.aqua[3],
+      //   borderColor: chartColors.aqua[3],
+      //   pointStyle: "rect",
+      // },
+      // {
+      //   type: "bar",
+      //   stack: "bar-stacked",
+      //   label: "VCT",
+      //   data: [10, 15, 20, 8, 5, 25, 18, 30, 12, 3, 15, 10],
+      //   backgroundColor: chartColors.purple[4],
+      //   borderColor: chartColors.purple[4],
+      //   pointStyle: "rect",
+      // },
+      // {
+      //   type: "bar",
+      //   stack: "bar-stacked",
+      //   label: "O2",
+      //   data: [15, 10, 8, 20, 18, 5, 12, 25, 30, 3, 15, 10],
+      //   backgroundColor: chartColors.green[3],
+      //   borderColor: chartColors.green[3],
+      //   pointStyle: "rect",
+      // },
+      // {
+      //   type: "bar",
+      //   stack: "bar-stacked",
+      //   label: "ETB",
+      //   data: [8, 15, 10, 5, 18, 20, 25, 30, 12, 3, 15, 10],
+      //   backgroundColor: chartColors.magenta[2],
+      //   borderColor: chartColors.magenta[2],
+      //   pointStyle: "rect",
+      // },
+      // {
+      //   type: "bar",
+      //   stack: "bar-stacked",
+      //   label: "VCT PARTS",
+      //   data: [10, 5, 20, 8, 12, 15, 18, 30, 3, 25, 15, 10],
+      //   backgroundColor: chartColors.blue[3],
+      //   borderColor: chartColors.blue[3],
+      //   pointStyle: "rect",
+      // },
+      // {
+      //   type: "bar",
+      //   stack: "bar-stacked",
+      //   label: "FP PARTS",
+      //   data: [5, 15, 10, 8, 12, 18, 20, 25, 30, 3, 15, 10],
+      //   backgroundColor: chartColors.brown[1],
+      //   borderColor: chartColors.brown[1],
+      //   pointStyle: "rect",
+      // },
+    ],
+  };
+
   return (
     <Box className="cell p-3 mb-3">
       <ChartTitleBar title="Mean Time to Repair (MTTR)" />
 
       <Box sx={{ height: { xs: "300px", md: "350px" } }}>
-        <Chart data={initialData} options={options} />
+        <Chart data={data} options={options} />
       </Box>
     </Box>
   );
