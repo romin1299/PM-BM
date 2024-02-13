ce850745628fec914e70b9387782c1ceb878f15d
mihir <mihir.osl@outlook.com>
2024-02-13 12:42:10 +0530

    Error fix: Row import error

--- client/src/KPI_Tab/MainPage/KPIBDHoursAndCountStatus.jsx
+++ client/src/KPI_Tab/MainPage/KPIBDHoursAndCountStatus.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {  Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const KPIBDHoursAndCountStatus = ({
   flagForTogglingFilter,
