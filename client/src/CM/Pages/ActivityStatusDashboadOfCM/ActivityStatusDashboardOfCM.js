e55ce1c2bd9d605c7889b18208ce5997f5b0229b
JD <jd.optimizesolutions@gmail.com>
2024-10-08 18:03:37 +0530

    DB url changes

--- client/src/CM/Pages/ActivityStatusDashboadOfCM/ActivityStatusDashboardOfCM.js
+++ client/src/CM/Pages/ActivityStatusDashboadOfCM/ActivityStatusDashboardOfCM.js
@@ -34,7 +34,6 @@ import {
 } from "../../../BM/Reports/ManHourReport/SubComponents/CommonFiltrationComponent";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { set } from "mongoose";
 import ExistingMachineReqSheetView from "../../Components/ReqestSheetOfCM/ExistingMachineRequestSheet/ExistingMachineReqSheetView";
 
 const ActivityStatusDashboardOfCM = () => {
@@ -458,7 +457,9 @@ const ActivityStatusDashboardOfCM = () => {
             </Modal.Header>
             <Modal.Body>
               <div>
-                <ExistingMachineReqSheetView cmSelectedSheetForView={cmSelectedSheetForView}/>
+                <ExistingMachineReqSheetView
+                  cmSelectedSheetForView={cmSelectedSheetForView}
+                />
               </div>
             </Modal.Body>
           </Modal>
